04eb97db647e1629d1ee228d501765f91178f747
SirAllrounder <98588165+SirAllrounder@users.noreply.github.com>
2022-04-21 01:31:19 +0530

    Add files via upload

---
+++ precache-manifest.c1925651634b7e9e8ec861ccd702de94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb060ea331ce16c84a519e8ed18d2be6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "481116206711605fbc95",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fa07a60507f616989bd8",
+    "url": "/static/js/2.d286ed04.chunk.js"
+  },
+  {
+    "revision": "7dc2529ea9c355027961799bc1ffe0c3",
+    "url": "/static/js/2.d286ed04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481116206711605fbc95",
+    "url": "/static/js/main.233ecc0a.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
